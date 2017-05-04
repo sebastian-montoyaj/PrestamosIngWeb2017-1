71cc48c3a7ba8947c56147aecd3ec4a991fedf36
Sebastián Montoya Jiménez <sebastian.montoyaj@udea.edu.co>
2017-05-03 19:59:58 -0500

    Se termino la logica del negocio para los usuarios y sus pruebas

--- PrestamosDispositivos/src/co/edu/udea/prestamos/bl/UsuarioBL.java
+++ PrestamosDispositivos/src/co/edu/udea/prestamos/bl/UsuarioBL.java
@@ -1,5 +1,110 @@
 package co.edu.udea.prestamos.bl;
 
-public class UsuarioBL {
+//Importes necesarios para la clase
+import org.springframework.transaction.annotation.Transactional;
 
+import co.edu.udea.prestamos.dao.interfaces.EstadoUsuarioDAO;
+import co.edu.udea.prestamos.dao.interfaces.UsuarioDAO;
+import co.edu.udea.prestamos.dto.EstadoUsuario;
+import co.edu.udea.prestamos.dto.Usuario;
+import co.edu.udea.prestamos.excepcion.ExcepcionPrestamos;
+
+/**
+ * Logica del negocio para las funciones asociadas a los usuarios del sistema
+ * @author Sebastian Montoya Jimenez
+ * @version 1.0 - 03/05/2017
+ */
+@Transactional // Esto le indica a la clase que las operaciones que se van a hacer son transaccionales
+public class UsuarioBL
+{
+	// Variable que permitira enlazar/inyectar la informacion de un UsuarioDAO con esta clase
+	private UsuarioDAO userDAO;
+	
+	// Variable que permitira enlazar/inyectar la informacion de un EstadoUsuarioDAO con algun usuario
+	private EstadoUsuarioDAO estadoUsuarioDAO;
+	
+	// Metodos getter/setter para la variable userDAO
+	public UsuarioDAO getUserDAO() {
+		return userDAO;
+	}
+
+	public void setUserDAO(UsuarioDAO userDAO) {
+		this.userDAO = userDAO;
+	}
+	
+	// Metodos getter/setter para la variable estadoUsuarioDAO
+	public EstadoUsuarioDAO getEstadoUsuarioDAO() {
+		return estadoUsuarioDAO;
+	}
+
+	public void setEstadoUsuarioDAO(EstadoUsuarioDAO estadoUsuarioDAO) {
+		this.estadoUsuarioDAO = estadoUsuarioDAO;
+	}
+
+	/**
+	 * RFN1 - Metodo para validar el usuario que esta ingresando su identificador y contraseña.
+	 * @param identificador Campo con el identificador del usuario
+	 * @param password Campo con la contraseña del usuario
+	 * @return Valor logico que confirma o deniega si el usuario hace parte del sistema
+	 * @throws ExcepcionPrestamos Ocurre cuando uno o los dos parametros estan vacios
+	 */
+	public boolean esUsuarioValido(String identificador, String password) throws ExcepcionPrestamos
+	{
+		// En primer lugar revisamos que los campos si tengan informacion
+		if (identificador == null || "".equals(identificador))
+		{
+			throw new ExcepcionPrestamos("Usuario y contraseña incorrectos!");
+		}
+		
+		if (password == null || "".equals(password))
+		{
+			throw new ExcepcionPrestamos("Usuario y contraseña incorrectos!");
+		}
+				
+		// Ahora, creamos un objeto usuario que se inicializa con el resultado de la busqueda de tal usuario
+		Usuario user = userDAO.obtener(Integer.parseInt(identificador));
+		
+		// Luego, si el objeto usuario es nulo entonces este usuario no es valido y retorno falso como resultado
+		if (user == null)
+		{
+			return false;
+		}
+		
+		// Si el usuario dado correponde con uno de la base de datos entonces pasamos a comprobar que la contraseña sea correcta, sino lo es entonces tampoco es valido
+		if (!user.getClave().equals(password))
+		{
+			return false;
+		}
+		
+		// NOTA: No es correcto decir que el usuario es correcto pero la contraseña no, porque eso le facilita a usuarios malintecionados vulnerar el sistema
+		
+		// Finalmente, si tanto el identificador como el password pasaron las condiciones anteriores entonces retorno que el usuario es valido
+		return true;
+	}
+	
+	/**
+	 * RFN8 - Metodo para cambiar el estado (inhabilitar/habilitar) del usuario
+	 * @param identificador Campo con el identificador del usuario
+	 * @param nuevoEstado Campo con el codigo/numero del nuevo estado para el usuario
+	 * @throws ExcepcionPrestamos Ocurre cuando se tuvo un problema ya sea obtiendo el usuario, el estado o modificando este en la BD
+	 */
+	public void cambiarEstadoUsuario(String identificador, int nuevoEstado) throws ExcepcionPrestamos
+	{
+		try
+		{
+			// En primer lugar, recuperamos el usuario con el identificador dado
+			Usuario user = userDAO.obtener(Integer.parseInt(identificador));
+			
+			// Luego, obtenemos el estado especifico que se indico en los parametros
+			EstadoUsuario estadoHabilitado = estadoUsuarioDAO.obtener(nuevoEstado);
+			
+			// Y hacemos el cambio de estado al usuario y lo actualizamos en la base de datos
+			user.setEstado(estadoHabilitado);
+			userDAO.actualizar(user);
+		}
+		catch (Exception e) // En caso de error recuperamos la excepcion y la guardamos en el log
+		{
+			throw new ExcepcionPrestamos("Error cambiando el estado de usuario!", e);
+		}
+	}
 }
